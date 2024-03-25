4c87950cdfc5b0dac0414a024933f0b5dc3da732
matheus mozart <mmsnborges@gmail.com>
2024-03-25 13:23:34 -0400

    first commit

---
+++ src/main/java/br/com/gestaosalario/gestaoempresa/GestaoEmpresaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestaosalario.gestaoempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEmpresaApplication.class, args);
+	}
+
+}
