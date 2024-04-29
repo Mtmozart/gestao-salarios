a97b3d4d2d7bf7e25cad661744e1fa11b98308bc
matheus mozart <mmsnborges@gmail.com>
2024-04-29 12:54:20 -0400

    iniciando spring boot

---
+++ src/main/java/br/com/gestaosalario/gestaoempresa/application/service/AuthenticationService.java
@@ -0,0 +1,22 @@
+package br.com.gestaosalario.gestaoempresa.application.service;
+
+import br.com.gestaosalario.gestaoempresa.domain.repositorys.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email);
+    }
+}
--- src/main/java/br/com/gestaosalario/gestaoempresa/domain/repositorys/UserRepository.java
+++ src/main/java/br/com/gestaosalario/gestaoempresa/domain/repositorys/UserRepository.java
@@ -2,6 +2,8 @@ package br.com.gestaosalario.gestaoempresa.domain.repositorys;
 
 import br.com.gestaosalario.gestaoempresa.domain.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByEmail(String email);
 }
